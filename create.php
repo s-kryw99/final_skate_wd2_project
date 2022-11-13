7fc1de6894ecb315d1928845b1314411c9abb93d
s-kryw99 <115945369+s-kryw99@users.noreply.github.com>
2022-11-12 20:50:47 -0600

    Later night nov 12
    made a lot of headway since last commit. Most of previous complaints are dealt with. 
    persisting is the width length not properly taking decimals after toying with most of the db settings... this should be easy! its databasing! all this thing does is take numbers and strings! I am moving on to picture upload tasks now. 
    
    -must figure out how to make list items sortable.

--- create.php
+++ create.php
@@ -2,7 +2,8 @@
 /*
  * Name:Alex Fleming
  * Title: create.php
- * Description: This file has the code needed for using the "New Post" part of the website to make new posts.
+ * Description: This file has the code needed for using the "New Entry" part of the website to
+ * make new posts to skateboard database.
  *
  */
 
@@ -14,7 +15,7 @@ $error = filter_input(INPUT_GET, 'error', FILTER_SANITIZE_STRING);
 <html>
 <head>
   <meta charset="utf-8">
-  <title>J. Twotwo's blog - New Post ::</title>
+  <title>Deck Rating System ::</title>
   <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
@@ -27,23 +28,47 @@ $error = filter_input(INPUT_GET, 'error', FILTER_SANITIZE_STRING);
 <?php } ?>
 
     <div id="header">
-      <h1><a href="index.php">J. Twotwo Blog - New Post</a></h1>
+      <h1><a href="index.php">New Deck Entry Form</a></h1>
     </div>
     <ul id="menu">
       <li><a href="index.php">Home</a></li>
-      <li><a href="create.php" class='active'>New Post</a></li>
+      <li><a href="create.php" class='active'>New Entry</a></li>
     </ul>
-    <div id="all_blogs">
+    <div id="all_entries">
       <form action="process_post.php" method="post">
         <fieldset>
-          <legend>New Blog Post</legend>
+          <legend>New Deck Entry Form</legend>
           <p>
             <label for="title">Title</label>
             <input name="title" id="title" />
           </p>
           <p>
-            <label for="content">Content</label>
-            <textarea name="content" id="content"></textarea>
+            <label for="brand">Brand</label>
+            <input name="brand" id="brand" />
+          </p>
+          <p>
+            <label for="length">Length (in cm)</label>
+            <input name="length" id="length" />
+          </p>
+          <p>
+            <label for="width">Width (in cm)</label>
+            <input name="width" id="width" />
+          </p>
+          <p>
+            <label for="release_year">Release Year</label>
+            <input name="release_year" id="release_year" />
+          </p>
+          <p>
+            <label for="year_used">Year of Use</label>
+            <input name="year_used" id="year_used" />
+          </p>
+          <p>
+            <label for="rating">Rating</label>
+            <input name="rating" id="rating" />
+          </p>
+          <p>
+            <label for="notes">Notes</label>
+            <textarea name="notes" id="notes"></textarea>
           </p>
           <p>
             <input type="submit" name="command" value="Create" />
@@ -51,9 +76,7 @@ $error = filter_input(INPUT_GET, 'error', FILTER_SANITIZE_STRING);
         </fieldset>
       </form>
     </div>
-    <div id="footer">
-      2022 - No Rights
-    </div>
+    <div id="footer">2022 - Copyright™</div>
   </div>
 </body>
 </html>
